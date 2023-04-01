dcb645a7db8f8161613829149d317f8d7cfb0687
Sebastian Sandner <sebastian.sandner89@gmail.com>
2023-04-01 13:59:58 +0200

    Layout and one message.

--- test/clj/guestbook/db/core_test.clj
+++ test/clj/guestbook/db/core_test.clj
@@ -1,13 +1,11 @@
 (ns guestbook.db.core-test
-  (:require
-   [guestbook.db.core :refer [*db*] :as db]
-   [java-time.pre-java8]
-   [luminus-migrations.core :as migrations]
-   [clojure.test :refer :all]
-   [next.jdbc :as jdbc]
-   [guestbook.config :refer [env]]
-   [mount.core :as mount]))
-
+(:require [clojure.test :refer :all]
+          [guestbook.config :refer [env]]
+          [guestbook.db.core :refer [*db*] :as db]
+          [java-time.pre-java8]
+          [luminus-migrations.core :as migrations]
+          [mount.core :as mount]
+          [next.jdbc :as jdbc]))
 (use-fixtures
   :once
   (fn [f]
@@ -16,23 +14,15 @@
      #'guestbook.db.core/*db*)
     (migrations/migrate ["migrate"] (select-keys env [:database-url]))
     (f)))
-
-(deftest test-users
+(deftest test-messages
   (jdbc/with-transaction [t-conn *db* {:rollback-only true}]
-    (is (= 1 (db/create-user!
+    (is (= 1 (db/save-message!
               t-conn
-              {:id         "1"
-               :first_name "Sam"
-               :last_name  "Smith"
-               :email      "sam.smith@example.com"
-               :pass       "pass"}
-              {})))
-    (is (= {:id         "1"
-            :first_name "Sam"
-            :last_name  "Smith"
-            :email      "sam.smith@example.com"
-            :pass       "pass"
-            :admin      nil
-            :last_login nil
-            :is_active  nil}
-           (db/get-user t-conn {:id "1"} {})))))
+              {:name "Bob"
+               :message "Hello, World"}
+              {:connection t-conn})))
+    (is (= {:name "Bob"
+            :message "Hello, World"}
+           (-> (db/get-messages t-conn {})
+               (first)
+               (select-keys [:name :message]))))))
