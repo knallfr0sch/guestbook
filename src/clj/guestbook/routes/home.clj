e17258c6f681e5c056ef09e5a5069251ef685583
Sebastian Sandner <sebastian.sandner89@gmail.com>
2023-04-01 14:13:05 +0200

    Saving messages is now possible.

--- src/clj/guestbook/routes/home.clj
+++ src/clj/guestbook/routes/home.clj
@@ -2,9 +2,9 @@
   (:require [guestbook.db.core :as db]
             [guestbook.layout :as layout]
             [guestbook.middleware :as middleware]
+            [ring.util.http-response :as response]
             [ring.util.response]))
- 
-
+  
 (defn home-page [request]
   (layout/render 
    request "home.html" {:messages (db/get-messages)}))
@@ -12,10 +12,15 @@
 (defn about-page [request]
   (layout/render request "about.html"))
 
+(defn save-message! [{:keys [params]}]
+  (db/save-message! params)
+  (response/found "/"))
+
 (defn home-routes []
   [""
    {:middleware [middleware/wrap-csrf
                  middleware/wrap-formats]}
    ["/" {:get home-page}]
-   ["/about" {:get about-page}]])
+   ["/about" {:get about-page}]
+   ["/message" {:post save-message!}]])
 
